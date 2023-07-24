ea708ebdebf8ab897166302e9564680bd9bd9f46
TheCodeMonk99 <rugutkclinton@gmail.com>
2023-07-24 17:56:37 +0000

    Trial 1

--- main.h
+++ main.h
@@ -1,4 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#define BUFF_SIZE 1024
+int _printf(const char *format, ...);
+void print_buffer(char buffer [], int *buff_ind);
 #endif
---
+++ x.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+#include <stdarg.h>
+#include <stddef.h>
+/**
+  * main - entry point
+  * Return: Always 0
+  */
+
+int main(void)
+{
+	char s[] = "I am happy to be here";
+	char b = 'a';
+
+	_printf("Serious\n");
+
+	printf("OUT OF _PRINTF\n");
+	_printf("Serious%%b\n");
+ 	printf("Serious%%b\n");
+	_printf("True crime: %c and %s plus\n ", b, s);
+	printf("True crime: %c and %s plus\n ", b, s);
+	return (0);
+}
