b7214eb51da81dac33b36284bda926eefef3e33c
lihandafabius <fabiuslihandaachevi@gmail.com>
2023-07-25 05:50:21 -0700

    second trial

---
+++ fun1.c
@@ -0,0 +1,4 @@
+int print_unsigned(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_hexa(va_list types, char buffer[], int flags, int width, int precision, int size);
