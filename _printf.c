fa5773212d313983cd0606a19a5e6d345a2aaf27
TheCodeMonk99 <rugutkclinton@gmail.com>
2023-07-25 09:39:03 +0000

    First half

---
+++ _printf.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
+/**
+  * _printf - Initialized printf
+  * @format: format
+  * @...: Additional arguments
+  * Return: number of printed char
+  */
+int _printf(const char *format, ...)
+{
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list args;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, args);
+			precision = get_precision(format, &i, args);
+			size = get_size(format, &i);
+			i++;
+			printed = handle_print(format, &i, args, buffer, flags,
+					width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+	print_buffer(buffer, &buff_ind);
+	va_end(args);
+	return (printed_chars);
+}
+
+/**
+  * print_buffer - prints contents of buffer if it exists
+  * @buffer: Arrays of chars to be printed
+  * @buff_ind: index of elemets
+  * Return: Always success
+  */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+	*buff_ind = 0;
+}
